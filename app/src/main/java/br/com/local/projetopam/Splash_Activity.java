fb4de5fc95d06e8b6ef2546d5ae4ce4a543c8c92
Natan Riegler <natan.riegler.silva@gmail.com>
2021-05-10 17:38:46 -0300

    Action Bar

---
+++ app/src/main/java/br/com/local/projetopam/Splash_Activity.java
@@ -0,0 +1,27 @@
+package br.com.local.projetopam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(getApplicationContext(), login_activity.class));
+                finish();
+
+            }
+        }, 3000);
+
+    }
+}
